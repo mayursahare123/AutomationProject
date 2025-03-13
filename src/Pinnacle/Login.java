6b244e4d8cf3d2eb4abc5b114254eb1de9b84256
unknown <mayur.sahare@pinnacle.in>
2025-03-13 18:35:01 +0530

    code push to master

---
+++ src/Pinnacle/Login.java
@@ -0,0 +1,13 @@
+package Pinnacle;
+
+public class Login {
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.Chrome.driver", "C:\\Users\\Mayur_Sahare\\Documents\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	}
+	
+	
+
+}
